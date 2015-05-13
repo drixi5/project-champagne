b893328bef27c59aac8493a25e341baa85dfb129
Hodzic Fiona <hodzicfiona@MacBook-Air-de-Hodzic.local>
2015-05-13 12:07:07 +0200

    fix file missing

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,14 @@
 
 # --- !Ups
 
+create table product (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  type                      varchar(255),
+  constraint uq_product_name unique (name),
+  constraint pk_product primary key (id))
+;
+
 create table token (
   token                     varchar(255) not null,
   user_id                   bigint,
@@ -32,6 +40,8 @@ create table user (
 # --- !Downs
 
 SET FOREIGN_KEY_CHECKS=0;
+
+drop table product;
 
 drop table token;
 
