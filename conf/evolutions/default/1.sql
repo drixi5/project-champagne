91169f5671813c4edfe584fcdb590dccf8a1056d
Xavier <xavier.d971@gmail.com>
2015-05-16 18:02:14 +0200

    Improve froms design & employee section added

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,24 +3,12 @@
 
 # --- !Ups
 
-<<<<<<< HEAD
-create table product (
-  id                        bigint auto_increment not null,
-  name                      varchar(255),
-  type                      varchar(255),
-  constraint uq_product_name unique (name),
-  constraint pk_product primary key (id))
-=======
-<<<<<<< HEAD
-create table stores (
-  id                        bigint auto_increment not null,
-  name                      varchar(255),
-  adresse1                  varchar(255),
-  adresse2                  varchar(255),
-  code_postal               integer,
-  ville                     varchar(255),
-  constraint pk_stores primary key (id))
-=======
+create table model_employee (
+  stock_id                  bigint auto_increment not null,
+  entity_id                 bigint,
+  constraint pk_model_employee primary key (stock_id))
+;
+
 create table stock (
   stock_id                  bigint auto_increment not null,
   entity_id                 bigint,
@@ -33,8 +21,16 @@ create table stock (
   date_creation             datetime,
   constraint uq_stock_name unique (name),
   constraint pk_stock primary key (stock_id))
->>>>>>> master
->>>>>>> master
+;
+
+create table stores (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  adresse1                  varchar(255),
+  adresse2                  varchar(255),
+  code_postal               integer,
+  villes                    varchar(255),
+  constraint pk_stores primary key (id))
 ;
 
 create table token (
@@ -67,15 +63,11 @@ create table user (
 
 SET FOREIGN_KEY_CHECKS=0;
 
-<<<<<<< HEAD
-drop table product;
-=======
-<<<<<<< HEAD
-drop table stores;
-=======
+drop table model_employee;
+
 drop table stock;
->>>>>>> master
->>>>>>> master
+
+drop table stores;
 
 drop table token;
 
