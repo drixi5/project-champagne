268fb349644a2a3a2dc74847ed711daf471e577f
Hodzic Fiona <fiona92130>
2015-06-21 16:54:38 +0200

    check form add product modification model.products/controlle.product/view.addProduct

--- app/controllers/Product.java
+++ app/controllers/Product.java
@@ -3,15 +3,18 @@ package controllers;
 
 import models.StoresProducts;
 import models.Stores;
+import models.TypeProduct;
 import models.User;
 import models.Products;
-
+import models.utils.AppException;
+import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import play.data.Form;
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
-
+import views.html.addStore;
 import views.html.product;
 import views.html.addProduct;
 
@@ -28,15 +31,21 @@ public class Product extends Controller {
     }
 	
 	final static Form<Products> productForm = form(Products.class);
-    
+	
     public static Result addProduct(){
+    
     	return ok(addProduct.render(User.findByEmail(request().username()), productForm));
     }
 	
 	public static Result saveProduct(){
-		Form<Products> form = productForm.bindFromRequest();
-		Products product= form.get();
-		product.save();
+		Form<Products> productForm = form(Products.class).bindFromRequest();
+		 if (productForm.hasErrors()){
+			 
+			 return badRequest(addProduct.render(User.findByEmail(request().username()), productForm));
+		 }
+		
+		 Products product= productForm.get();
+		 product.save();
 		return index();
 	}
 	
@@ -45,4 +54,31 @@ public class Product extends Controller {
 		return ok(toJson(products));
 	}
 	
+	public static class addNewProduct {
+
+        @Constraints.Required
+        public String name;
+        
+        @Constraints.Required
+        public TypeProduct typeProduct;
+
+        /**
+         * Validate the authentication.
+         *
+         * @return null if validation ok, string with details otherwise
+         */
+
+	
+	public String validate() {
+        if (isBlank(name)) {
+            return "Name is required";
+        }
+
+        return null;
+	}
+    private boolean isBlank(String input) {
+        return input == null || input.isEmpty() || input.trim().isEmpty();
+    }
+	
+	}	
 }
