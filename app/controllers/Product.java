607f68671ec5ac943b1b86b6a1027e4cca4fffa0
Hodzic Fiona <fiona92130>
2015-06-22 12:30:58 +0200

    update product

--- app/controllers/Product.java
+++ app/controllers/Product.java
@@ -12,9 +12,12 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import play.data.Form;
+import play.data.format.Formats;
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
 import views.html.addStore;
+import views.html.editStore;
+import views.html.editProduct;
 import views.html.product;
 import views.html.addProduct;
 
@@ -33,15 +36,15 @@ public class Product extends Controller {
 	final static Form<Products> productForm = form(Products.class);
 	
     public static Result addProduct(){
-    
-    	return ok(addProduct.render(User.findByEmail(request().username()), productForm));
+    	Products product = new Products();
+    	return ok(addProduct.render(User.findByEmail(request().username()), productForm, product, routes.Product.addProduct()));
     }
 	
 	public static Result saveProduct(){
 		Form<Products> productForm = form(Products.class).bindFromRequest();
 		 if (productForm.hasErrors()){
-			 
-			 return badRequest(addProduct.render(User.findByEmail(request().username()), productForm));
+			 Products product = new Products();
+			 return badRequest(addProduct.render(User.findByEmail(request().username()), productForm, product, routes.Product.addProduct()));
 		 }
 		
 		 Products product= productForm.get();
@@ -57,10 +60,11 @@ public class Product extends Controller {
 	public static class addNewProduct {
 
         @Constraints.Required
+        @Formats.NonEmpty
         public String name;
-        
-        @Constraints.Required
-        public TypeProduct typeProduct;
+	
+        //@Constraints.Required
+        //public TypeProduct typeProduct;
 
         /**
          * Validate the authentication.
@@ -73,12 +77,42 @@ public class Product extends Controller {
         if (isBlank(name)) {
             return "Name is required";
         }
+        //if (isBlank(typeProduct)) {
+            //return "Type is required";
+        //}
 
         return null;
 	}
+	
     private boolean isBlank(String input) {
         return input == null || input.isEmpty() || input.trim().isEmpty();
+       
     }
-	
+	}
+    //private boolean isBlank(TypeProduct input) {
+        //return input == null || input.isEmpty() || input.trim().isEmpty();
+        
+    //}
+    
+	 public static Result edit(Long id)  {
+	        Products product = Products.find.byId(id);
+	        
+	        return ok(editProduct.render(User.findByEmail(request().username()),productForm.fill(product), product));
+	    }
+	 
+	 public static Result update(Long id)  {
+		 	Form<Products> filledForm = productForm.bindFromRequest();
+
+	        /*if(filledForm.hasErrors())  {
+	            //can't pull out of the form if there are errors
+	            Book book = Book.find.byId(id);
+	            return ok(edit.render(filledForm, book));
+	        } else {*/
+	            Products product = filledForm.get();
+	            product.update(id);
+	            return index();
+	        
+	    }
+	 
 	}	
-}
+
